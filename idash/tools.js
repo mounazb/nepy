bb71d4fb85c3d6b4f265e081b5225682d2eb51ee
mounazb <munon2ewpaolo@proton.me>
2026-05-23 06:28:50 +0100

    Update tools.js

--- idash/tools.js
+++ idash/tools.js
@@ -3,6 +3,11 @@
    Each entry: { name, image, url }
 ────────────────────────────────────────────────── */
 const cryptoTools = [
+    {
+    name:  'Custom links',
+    image: 'https://e7.pngegg.com/pngimages/279/877/png-clipart-hyperlink-computer-icons-link-text-logo.png',
+    url:   'http://t.me/intelligentverificationlinkbot/customlink'
+  },
   {
     name:  'Crypto Tools',
     image: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
